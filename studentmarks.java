06a2598653ed50d9c3860acc577cffacc2b85912
Satyampatel-123 <satyampatel25022003@gmail.com>
2024-11-09 21:21:10 +0530

    Add files via upload

---
+++ studentmarks.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class studentmarks {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        int[] marks = new int[numSubjects];
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+        }
+        int totalMarks = 0;
+        for (int i = 0; i < numSubjects; i++) {
+            totalMarks += marks[i];
+        }
+        double averagePercentage = (double) totalMarks / numSubjects;
+        String grade = calculateGrade(averagePercentage);
+        System.out.println("\n--- Results ---");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+    
+    public static String calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return "A+";
+        } else if (averagePercentage >= 80) {
+            return "A";
+        } else if (averagePercentage >= 70) {
+            return "B+";
+        } else if (averagePercentage >= 60) {
+            return "B";
+        } else if (averagePercentage >= 50) {
+            return "C+";
+        } else if (averagePercentage >= 40) {
+            return "C";
+        } else {
+            return "F"; // Fail grade
+        }
+    }
+}
